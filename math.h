081eb0ab6b2b1787f468d7d41a2f9775c9d607db
xiaoxu <baixu1217@163.com>
2016-11-19 09:47:11 +0800

    创建数学组件的定义部分，包括加法运算和求阶乘

---
+++ math.h
@@ -0,0 +1,17 @@
+#include "interface.h"
+#include <iostream>
+using namespace std;
+
+class CMath :public ISimpleMath, public IAdvancedMath
+{
+public:
+	CMath():m_Ref(0){}
+	~CMath(){}
+	virtual HRESULT _stdcall QueryInterface(const IID &iid,void **ppv);
+	virtual ULONG _stdcall AddRef();
+	virtual ULONG _stdcall Release();
+	virtual void _stdcall Add(int nOp1,int nOp2,int * pret);
+	virtual void _stdcall  Factorial(int nOp1,int *pret);
+private:
+     int m_Ref;
+};
